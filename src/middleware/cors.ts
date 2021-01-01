1bd60b4110fb47f1243696e0f8081ac447a85c87
Donovan Daniels <dwddaniels262@gmail.com>
2020-12-31 19:17:31 -0600

    Fix capitalization of header

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,7 +3,7 @@ import type { Middleware } from '../structures';
 const mod: Middleware = () => {
   return (_, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.setHeader('Access-COntrol-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     next();
   };
